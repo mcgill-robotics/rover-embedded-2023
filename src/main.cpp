d43307dfbe1b6d3228bfa1eb4cc478cf4125ea5d
vboucher77 <76258574+vboucher77@users.noreply.github.com>
2022-11-06 19:59:40 -0500

    added code for timer

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,16 @@
 #include "lower_arm_main.h"
 #include "power_main.h"
 
+#define loop_hertz 100
+volatile byte tflag;
+
 void setup() {
+  TIM_TypeDef *Instance = TIM1;
+  HardwareTimer *loop_timer = new HardwareTimer(Instance);
+
+  loop_timer->setOverflow(loop_hertz, HERTZ_FORMAT);
+  loop_timer->attachInterrupt(loop_timer_int);
+  loop_timer->resume();
   #ifdef SCIENCE
   science_setup();
   #endif
@@ -28,22 +37,29 @@ void setup() {
 }
 
 void loop() {
-  #ifdef SCIENCE
-  science_loop();
-  #endif
-  #ifdef POWER
-  power_loop();
-  #endif
-  #ifdef SENSOR
-  sensor_loop();
-  #endif
-  #ifdef UPPER_ARM
-  upper_arm_loop();
-  #endif
-  #ifdef LOWER_ARM
-  lower_arm_loop();
-  #endif
-  #ifdef DRIVE
-  drive_loop();
-  #endif
+  if(tflag == 1){
+    #ifdef SCIENCE
+    science_loop();
+    #endif
+    #ifdef POWER
+    power_loop();
+    #endif
+    #ifdef SENSOR
+    sensor_loop();
+    #endif
+    #ifdef UPPER_ARM
+    upper_arm_loop();
+    #endif
+    #ifdef LOWER_ARM
+    lower_arm_loop();
+    #endif
+    #ifdef DRIVE
+    drive_loop();
+    #endif
+    tflag = 0;
+  }
+}
+
+void loop_timer_int(){
+  tflag = 1;
 }
