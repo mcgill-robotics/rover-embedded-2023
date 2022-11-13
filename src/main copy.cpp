c39fe7bc44c162716f0185d1652df0e1c5bd8a11
Aman Sidhu <amansidhuvan@gmail.com>
2022-11-13 14:48:02 -0500

    Testing motor controller firmware

---
+++ src/main copy.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#include "CytronMotorDriver.h"
+
+
+// Configure the motor driver.
+CytronMD motor(PWM_DIR, 3, 4);  // PWM = Pin 3, DIR = Pin 4.
+
+
+// The setup routine runs once when you press reset.
+void setup() {
+  
+}
+
+
+// The loop routine runs over and over again forever.
+void loop() {
+  motor.setSpeed(128);  // Run forward at 50% speed.
+  delay(1000);
+  
+  motor.setSpeed(255);  // Run forward at full speed.
+  delay(1000);
+
+  motor.setSpeed(0);    // Stop.
+  delay(1000);
+
+  motor.setSpeed(-128);  // Run backward at 50% speed.
+  delay(1000);
+  
+  motor.setSpeed(-255);  // Run backward at full speed.
+  delay(1000);
+
+  motor.setSpeed(0);    // Stop.
+  delay(1000);
+}
