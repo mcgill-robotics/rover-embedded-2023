326679a5cd8c4f7335d4b3b51580f3a5247e46fc
vboucher77 <76258574+vboucher77@users.noreply.github.com>
2022-12-02 17:40:53 -0500

    pushing

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,19 @@
 #include "upper_arm_main.h"
 #include "lower_arm_main.h"
 #include "power_main.h"
+#include "Can.h"
 
 #define loop_hertz 100
-volatile byte tflag;
-void loop_timer_int();
+void canbus_timer_int();
 
 void setup() {
   TIM_TypeDef *Instance = TIM1;
-  HardwareTimer *loop_timer = new HardwareTimer(Instance);
+  HardwareTimer *canbus_timer = new HardwareTimer(Instance);
 
-  loop_timer->setOverflow(loop_hertz, HERTZ_FORMAT);
-  loop_timer->attachInterrupt(loop_timer_int);
-  loop_timer->resume();
+  canbus_timer->setOverflow(loop_hertz, HERTZ_FORMAT);
+  canbus_timer->attachInterrupt(canbus_timer_int);
+
+  CANBus::Message write_buffer[25];
   #ifdef SCIENCE
   science_setup();
   #endif
@@ -35,32 +36,30 @@ void setup() {
   #ifdef DRIVE
   drive_setup();
   #endif
+  canbus_timer->resume();
 }
 
 void loop() {
-  if(tflag == 1){
-    #ifdef SCIENCE
-    science_loop();
-    #endif
-    #ifdef POWER
-    power_loop();
-    #endif
-    #ifdef SENSOR
-    sensor_loop();
-    #endif
-    #ifdef UPPER_ARM
-    upper_arm_loop();
-    #endif
-    #ifdef LOWER_ARM
-    lower_arm_loop();
-    #endif
-    #ifdef DRIVE
-    drive_loop();
-    #endif
-    tflag = 0;
-  }
+  #ifdef SCIENCE
+  science_loop();
+  #endif
+  #ifdef POWER
+  power_loop();
+  #endif
+  #ifdef SENSOR
+  sensor_loop();
+  #endif
+  #ifdef UPPER_ARM
+  upper_arm_loop();
+  #endif
+  #ifdef LOWER_ARM
+  lower_arm_loop();
+  #endif
+  #ifdef DRIVE
+  drive_loop();
+  #endif
 }
 
 void loop_timer_int(){
-  tflag = 1;
+  
 }
