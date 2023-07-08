782f9e10a253f9a1c9a2c48f3e353c708c770c89
tina-zhang18 <118072600+tina-zhang18@users.noreply.github.com>
2023-07-08 14:51:24 -0400

    Stepper code

--- src/science.cpp
+++ src/science.cpp
@@ -2,13 +2,30 @@
 
 #include <Arduino.h>
 #include "science.h"
+#include "DRV8834.h"
 
-void science_setup() {
-  // put your setup code here, to run once:
+#define MOTOR_STEPS 200
+
+#define DIR 8
+#define STEP 9
+#define MS1 10
+#define MS2 11
+#define MS3 12
+DRV8834 stepper(MOTOR_STEPS, DIR, STEP, MS1, MS2, MS3);
+
+/*void writeToStepperMotor(int steps, int direction) {
+  //If direction = 1 --> CW
+  //If direction = -1 --> CCW
+  stepper.rotate(steps*direction)*/
+
+void science_setup () {
+  SerialUSB.begin(9600);
+  stepper.begin(30,1);
 }
 
-void science_loop() {
-  // put your main code here, to run repeatedly:
+void science_loop () {
+  SerialUSB.println("Upload");
+  stepper.rotate(360);
 }
 
 #endif //LEAVE THIS AT THE BOTTOM OF THIS FILE
