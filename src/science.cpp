2a8b91bb7db12a9232915c1591c94fb0787f9522
Sam Dayo <samueldayo2017@gmail.com>
2023-07-08 16:44:39 -0400

    Science test

--- src/science.cpp
+++ src/science.cpp
@@ -1,17 +1,18 @@
-#ifdef SCIENCE //LEAVE THIS AT THE TOP OF THIS FILE
+//#ifdef SCIENCE //LEAVE THIS AT THE TOP OF THIS FILE
 
 #include <Arduino.h>
 #include "science.h"
 #include "DRV8834.h"
 
 #define MOTOR_STEPS 200
+#define RPM 75
 
 #define DIR 8
 #define STEP 9
-#define MS1 10
-#define MS2 11
-#define MS3 12
-DRV8834 stepper(MOTOR_STEPS, DIR, STEP, MS1, MS2, MS3);
+#define M0 10
+#define M1 11
+
+DRV8834 stepper(MOTOR_STEPS, DIR, STEP, M0, M1);
 
 /*void writeToStepperMotor(int steps, int direction) {
   //If direction = 1 --> CW
@@ -19,13 +20,27 @@ DRV8834 stepper(MOTOR_STEPS, DIR, STEP, MS1, MS2, MS3);
   stepper.rotate(steps*direction)*/
 
 void science_setup () {
-  SerialUSB.begin(9600);
-  stepper.begin(30,1);
+ // SerialUSB.begin(9600);
+  //stepper.begin(RPM, 1);
+  //stepper.enable();
+  pinMode(DIR, OUTPUT); 
+  pinMode(STEP, OUTPUT);
+  pinMode(M0, OUTPUT);
+  pinMode(M1, OUTPUT);
+
+  digitalWrite(M0, LOW);
+  digitalWrite(M1, HIGH);
+  digitalWrite(DIR, HIGH); 
+
 }
 
 void science_loop () {
-  SerialUSB.println("Upload");
-  stepper.rotate(360);
+  for (int i = 0; i < MOTOR_STEPS; i++){
+    digitalWrite(STEP, HIGH);
+    delay(5); 
+    digitalWrite(STEP, LOW); 
+    delay(5);
+  }
 }
 
-#endif //LEAVE THIS AT THE BOTTOM OF THIS FILE
+//#endif //LEAVE THIS AT THE BOTTOM OF THIS FILE
