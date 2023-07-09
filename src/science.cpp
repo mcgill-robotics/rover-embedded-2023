6eeb72ad87837beaf0139af414476c721162e41b
RUi Yi Wang <rui.y.wang@mail.mcgill.ca>
2023-07-08 22:14:28 -0400

    turns smoothly but tuns both ways, Vref around 0.6

--- src/science.cpp
+++ src/science.cpp
@@ -14,33 +14,46 @@
 
 DRV8834 stepper(MOTOR_STEPS, DIR, STEP, M0, M1);
 
-/*void writeToStepperMotor(int steps, int direction) {
+void writeToStepperMotor(int steps, int direction) {
   //If direction = 1 --> CW
   //If direction = -1 --> CCW
-  stepper.rotate(steps*direction)*/
+  stepper.move(direction*steps);
+}
 
 void science_setup () {
- // SerialUSB.begin(9600);
-  //stepper.begin(RPM, 1);
-  //stepper.enable();
-  pinMode(DIR, OUTPUT); 
+  //SerialUSB.begin(9600);
+  stepper.begin(RPM);
+  //stepper.setEnableActiveState(LOW);
+  stepper.enable();
+
+  /*pinMode(DIR, OUTPUT); 
   pinMode(STEP, OUTPUT);
   pinMode(M0, OUTPUT);
   pinMode(M1, OUTPUT);
-
+  
   digitalWrite(M0, LOW);
   digitalWrite(M1, HIGH);
-  digitalWrite(DIR, HIGH); 
+  digitalWrite(DIR, HIGH); */
 
 }
 
 void science_loop () {
-  for (int i = 0; i < MOTOR_STEPS; i++){
-    digitalWrite(STEP, HIGH);
-    delay(5); 
-    digitalWrite(STEP, LOW); 
-    delay(5);
-  }
+  //writeToStepperMotor(1800,1);
+  stepper.setMicrostep(1);
+
+  stepper.rotate(-720);     // forward revolution
+  //stepper.rotate(-60);
+
+  //stepper.move(MOTOR_STEPS);    // forward revolution
+  //stepper.move(-MOTOR_STEPS);
+  //writeToStepperMotor(200,-1);
+  //SerialUSB.println("2");
+  //delay(1000);
+  /*digitalWrite(STEP, HIGH);
+  delay(5); 
+  digitalWrite(STEP, LOW); 
+  delay(5);*/
+  delay(100);
 }
 
 //#endif //LEAVE THIS AT THE BOTTOM OF THIS FILE
