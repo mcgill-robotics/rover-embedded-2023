47ec22ebf137af8d7f2cfc68acf5992247b6f50b
vboucher07 <97803777+vboucher07@users.noreply.github.com>
2023-07-20 20:51:37 -0400

    limit switches added

--- src/science.cpp
+++ src/science.cpp
@@ -1,4 +1,4 @@
-#ifdef SCIENCE //LEAVE THIS AT THE TOP OF THIS FILE
+// #ifdef SCIENCE //LEAVE THIS AT THE TOP OF THIS FILE
 
 #include <Arduino.h>
 #include "science.h"
@@ -14,6 +14,8 @@ float scienceFeedback[5];
 AccelStepper stepper(AccelStepper::DRIVER, STEP, DIR);
 IntervalTimer stepperTimer;
 
+int leadScrewState = 0;
+
 CytronMD MOT1(PWM_DIR, MOT1_PWM, MOT1_DIR);
 CytronMD MOT2(PWM_DIR, MOT2_PWM, MOT2_DIR);
 
@@ -21,6 +23,12 @@ ros::NodeHandle nh;
 std_msgs::Float32MultiArray scienceFBMsg;
 std_msgs::Float32MultiArray scienceCmdMsg;
 ros::Publisher scienceFB("scienceFB", &scienceFBMsg);
+
+enum Limits {
+  off = 0,
+  up = 1,
+  down = -1
+};
 
 void scienceCB(const std_msgs::Float32MultiArray& input_msg){
   scienceTargets[0] = input_msg.data[0];
@@ -35,17 +43,21 @@ void stepperISR(){
 } 
 
 void readAllMoistures(){
-  scienceFeedback[0] = analogRead(MOIST_1) * (100.0/4095.0);
-  scienceFeedback[1] = analogRead(MOIST_2) * (100.0/4095.0);
-  scienceFeedback[2] = analogRead(MOIST_3) * (100.0/4095.0);
-  scienceFeedback[3] = analogRead(MOIST_4) * (100.0/4095.0);
+  scienceFeedback[0] = analogRead(MOIST_1) * (89.189189189/4095.0);
+  scienceFeedback[1] = analogRead(MOIST_2) * (89.189189189/4095.0);
+  scienceFeedback[2] = analogRead(MOIST_3) * (89.189189189/4095.0);
+  scienceFeedback[3] = analogRead(MOIST_4) * (89.189189189/4095.0);
   scienceFeedback[4] = stepper.currentPosition() / GEAR_RATIO;
 }
 
 void writeAllMotors(){
   stepper.move(scienceTargets[0] * GEAR_RATIO);
   MOT1.setSpeed(scienceTargets[1]);
-  MOT2.setSpeed(scienceTargets[2]);
+  float motorSpeed = scienceTargets[2];
+  if((leadScrewState == Limits::down && motorSpeed < 0) || (leadScrewState == Limits::up && motorSpeed > 0)){
+    motorSpeed = 0;
+  }
+  MOT2.setSpeed(motorSpeed);
 }
 
 void science_setup () {
@@ -57,6 +69,8 @@ void science_setup () {
 
   stepper.setMaxSpeed(1000.0);
   stepper.setAcceleration(200.0);
+  attachInterrupt(LIM_1, lim1ISR, FALLING);
+  attachInterrupt(LIM_2, lim2ISR, FALLING);
   // stepper.moveTo(200);
   stepperTimer.begin(stepperISR, 1000000/STEPPER_FREQ);
 
@@ -73,6 +87,10 @@ void science_setup () {
 }
 
 void science_loop () {
+  if(digitalRead(LIM_1) == 1 && digitalRead(LIM_2) == 1){
+    leadScrewState = Limits::off;
+  }
+
   readAllMoistures();
 
   writeAllMotors();
@@ -82,6 +100,13 @@ void science_loop () {
   delay(5);
 }
 
+void lim1ISR(){
+  leadScrewState = Limits::up;
+}
+
+void lim2ISR(){
+  leadScrewState = Limits::down;
+}
 
 
-#endif //LEAVE THIS AT THE BOTTOM OF THIS FILE
+// #endif //LEAVE THIS AT THE BOTTOM OF THIS FILE
